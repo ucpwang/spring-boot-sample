fd60afe823ddeec1ead501bd368b78fc0f0cb595
Jacob(You-Hyun Hwang) <ghkddbguse@gmail.com>
2016-09-28 01:48:18 +0900

    ApplicationProperties 코드 정리

--- src/main/java/io/github/ucpwang/sample/support/ApplicationProperties.java
+++ src/main/java/io/github/ucpwang/sample/support/ApplicationProperties.java
@@ -1,25 +1,17 @@
 package io.github.ucpwang.sample.support;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@Data
 @ConfigurationProperties(prefix = "sample.props")
 public class ApplicationProperties {
 
-    @NestedConfigurationProperty
     private List<Map> admins = new ArrayList<>();
-    public List<Map> getAdmins() {
-        return this.admins;
-    }
-
-    @NestedConfigurationProperty
     private List<String> testmsg = new ArrayList<>();
-    public List<String> getTestmsg() {
-        return this.testmsg;
-    }
 
 }
